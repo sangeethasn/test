5ded64f7ec646fe557944096af7695fb9efaa0e4
sangeethasn <sangeetha.sn@inapp.com>
2018-07-20 17:34:04 +0530

    adding binary search program

---
+++ bsearch.py
@@ -0,0 +1,28 @@
+print(binarySearch( [0, 1, 2, 8, 13, 17, 19, 32, 42,], 3))
+#print(binarySearch(testlist, 13))
+
+
+
+
+def binarySearch(alist,item):
+ first = 0
+ last = len(alist)-1
+ found = False
+ while first<=last and not found:
+      midpoint = (first + last)//2
+      if alist[midpoint] == item:
+         found = True
+      else:
+        if item < alist[midpoint]:
+         last = midpoint-1
+        else:
+         first = midpoint+1
+ return found
+
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+
+
+
+
+
+#binarySearch(alist[:midpoint],item)
