76e0aadbec94a09383d66c06b47ef0e6b1f35a4d
sangeethasn <sangeetha.sn@inapp.com>
2018-07-20 17:38:47 +0530

    adding program to count the number of words in a sentence

---
+++ count_words.py
@@ -0,0 +1,9 @@
+file=open('countfile.txt','r+')
+wordcount={}
+for word in file.read().split():
+    if word not in wordcount:
+        wordcount[word] = 1
+    else:
+        wordcount[word] += 1
+print (wordcount)
+file.close();
